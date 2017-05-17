da136d1e37c115cb6bf8b5eca4bdde4e8b30524b
MariaRodriguezPrieto1998 <lupitapri1998@hotmail.com>
2017-05-17 11:06:55 -0500

    PRACTICA 4 CALCULADORA
    En esta practica realizamos una calculadora en la cual nos da la funcion
    de suma , resta, multiplicacion y division .. Disculpe la tardanza
    profe. :)

---
+++ Lupitacalculadora.js
@@ -0,0 +1,29 @@
+
+function suma()
+{
+	var numero1 = Number(document.getElementById('N1').value)
+	 var numero2 = Number(document.getElementById('N2').value)
+	 
+       alert("El resultado de tu operacion es ="+(numero1+numero2));
+	  }
+function resta()
+{
+	var numero1 = Number(document.getElementById('N1').value)
+	 var numero2 = Number(document.getElementById('N2').value)
+	 
+       alert("El resultado de tu operacion es ="+(numero1-numero2));
+	  }
+function multiplicacion()
+{
+	var numero1 = Number(document.getElementById('N1').value)
+	 var numero2 = Number(document.getElementById('N2').value)
+	 
+       alert("El resultado de tu operacion es ="+(numero1*numero2));
+	  }
+function division()
+{
+	var numero1 = Number(document.getElementById('N1').value)
+	 var numero2 = Number(document.getElementById('N2').value)
+	 
+       alert("El resultado de tu operacion es ="+(numero1/numero2));
+	  }
